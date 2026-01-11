094158d74e57f6f1980211bfd8e9417dc03e7edf
Mide001 <olamideisreal5@gmail.com>
2026-01-11 01:42:25 +0100

    refactor: reset migrations for single api key architecture

--- Openly-Backend/prisma/migrations/20260107015247_add_auth_tokens/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Merchant" ADD COLUMN     "emailVerificationToken" TEXT,
-ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT true,
-ADD COLUMN     "passwordResetExpires" TIMESTAMP(3),
-ADD COLUMN     "passwordResetToken" TEXT;
